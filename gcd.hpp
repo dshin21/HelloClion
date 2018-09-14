5c830a75e1a55eb73143f9f6f9d9b9b82128b055
Daniel Shin <danielshin234@gmail.com>
2018-09-14 10:49:22 -0700

    hpp file

---
+++ gcd.hpp
@@ -0,0 +1,16 @@
+//
+// Created by danie on 2018-09-13.
+//
+
+#ifndef HELLOCLION_GCD_HPP
+#define HELLOCLION_GCD_HPP
+
+#endif //HELLOCLION_GCD_HPP
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd( const int a, const int b );
