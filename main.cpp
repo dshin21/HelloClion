38bce042c7da9709a9df417693aa85e194516033
Daniel Shin <danielshin234@gmail.com>
2018-09-13 21:01:47 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "gcd.hpp"
+
+int gcd( const int a, const int b ) {
+    return ( b == 0 ) ? a : gcd( b, a % b );
+}
 
 int main() {
-    std::cout << "Initial Commit" << std::endl;
+    std::cout << gcd( 33, 22 ) << std::endl;
     return 0;
 }
